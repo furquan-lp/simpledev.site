20f76f1df32925effb9c828b94bb79d9e3c124dc
Syed Furquan Ahmad <furquan@pm.me>
2022-04-22 23:47:13 +0530

    Added more placeholder links to the top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -8,6 +8,8 @@ const TopBar = () => {
       <ul>
         <Link to="/"><li>Home</li></Link>
         <Link to="/about"><li>About</li></Link>
+        <a href="https://tctp.xyz/"><li>Blog</li></a>
+        <Link to="/portfolio"><li>Portfolio</li></Link>
         <Link to="/contact"><li>Contact</li></Link>
       </ul>
     </div>
