e49684d72026f6dd63cb3d9cd808d8bf1b4d497e
Syed Furquan Ahmad <furquan@pm.me>
2022-04-23 00:41:59 +0530

    Added Link to / to HomeLogo

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -11,7 +11,7 @@ const HomeLogo = () =>
 const TopBar = () => {
   return (
     <div className="topBar">
-      <HomeLogo />
+      <Link to="/"><HomeLogo /></Link>
       <ul>
         <Link to="/"><li>Home</li></Link>
         <Link to="/about"><li>About</li></Link>
