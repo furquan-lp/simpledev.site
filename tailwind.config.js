3d35e5d7c04d2144ae54d54d40dbbb7a066c6d50
Syed F Ahmad <furquan@protonmail.com>
2022-11-28 20:29:56 +0530

    Require flowbite plugin and added flowbite-react to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -21,5 +22,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
