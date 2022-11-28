8dff564e628e548bf9721eee90b0670ca3f30188
Syed F Ahmad <furquan@protonmail.com>
2022-11-29 01:09:45 +0530

    Added Footer with copyright notice and GitHub repository link

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import { FiGithub } from 'react-icons/fi';
+
+const Footer = () => <div className="flex justify-between items-center bg-green-dark text-white p-2">
+  <code>Copyright (C) 2022 Syed Furquan Ahmad</code>
+  <a href="https://github.com/furquan-lp/simpledev.site"><FiGithub className="mx-2" /></a>
+</div>;
+
+export default Footer;
