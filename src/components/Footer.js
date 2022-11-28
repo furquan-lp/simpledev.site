895a9f6969e1926eba8ecf7e08b2316ff5abcecf
Syed F Ahmad <furquan@protonmail.com>
2022-11-29 01:23:12 +0530

    Use the copyright symbol instead of (C)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { FiGithub } from 'react-icons/fi';
 import { Tooltip } from 'flowbite-react';
 
 const Footer = () => <div className="flex justify-between items-center bg-green-dark text-white p-1.5">
-  <code>Copyright (C) 2022 Syed Furquan Ahmad</code>
+  Copyright &#169; 2022 Syed Furquan Ahmad
   <Tooltip content="Check the source code">
     <a href="https://github.com/furquan-lp/simpledev.site">
       <FiGithub className="mx-2 md:w-3/4 md:h-3/4 sm:w-fit sm:h-fit" />
