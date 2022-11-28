09b095c47aab8b4bcd6803c35ae08ffbb6b7589a
Syed F Ahmad <furquan@protonmail.com>
2022-11-28 22:53:28 +0530

    Added HiText component with basic 'About' info to display on the homepage

---
+++ src/components/Hi.js
@@ -0,0 +1,16 @@
+const HiText = () =>
+  <div className="px-11 py-5">
+    <div className="flex my-5 mr-5 px-5 py-10 bg-green-dark text-white rounded-md">
+      <div className="flex flex-col py-10 pr-5 pl-10 ">
+        <p className="md:text-5xl sm:text-xl my-1">Hi,</p>
+        <p className="md:text-5xl sm:text-xl my-1">I'm Syed</p>
+        <p className="md:text-5xl sm:text-xl my-1">Aspiring Web Developer</p>
+      </div>
+      <div className="mx-10 px-2 w-1 h-auto bg-green-light -skew-x-12"></div>
+      <div>
+        foo
+      </div>
+    </div>
+  </div>;
+
+export default HiText;
