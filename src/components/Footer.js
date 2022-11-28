a57e1fc4823834fbadebabe02e755e3e17df7078
Syed F Ahmad <furquan@protonmail.com>
2022-11-29 01:12:23 +0530

    Made FiGithub icon responsive and reduced padding for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 import { FiGithub } from 'react-icons/fi';
 
-const Footer = () => <div className="flex justify-between items-center bg-green-dark text-white p-2">
+const Footer = () => <div className="flex justify-between items-center bg-green-dark text-white p-1.5">
   <code>Copyright (C) 2022 Syed Furquan Ahmad</code>
-  <a href="https://github.com/furquan-lp/simpledev.site"><FiGithub className="mx-2" /></a>
+  <a href="https://github.com/furquan-lp/simpledev.site">
+    <FiGithub className="mx-2 md:w-3/4 md:h-3/4 sm:w-fit sm:h-fit" />
+  </a>
 </div>;
 
 export default Footer;
