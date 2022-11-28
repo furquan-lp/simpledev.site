ce689d795ba256105cfab6f945b067c88333af36
Syed F Ahmad <furquan@protonmail.com>
2022-11-29 00:54:15 +0530

    Added Welcome and HiCards components, added a content container below TopBar with margin and flex

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 
 import TopBar from './components/TopBar';
+import Welcome from './components/Welcome';
+import HiCards from './components/Hi';
 
 const App = () => {
   return (
     <div className="bg-mountains-blur bg-center bg-cover bg-no-repeat">
-      <div>
-        <TopBar />
-        <div className="font-sans font-bold text-white text-4xl hover:underline p-10 pb-96">Welcome!</div>
+      <TopBar />
+      <div className="flex flex-col mx-12">
+        <Welcome />
+        <HiCards />
+        <div className="font-sans font-bold text-white text-4xl hover:underline p-10 pb-20">Goodbye!</div>
       </div>
     </div>
   );
