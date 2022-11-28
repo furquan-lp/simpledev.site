b21c4987bbab68a4bdb894bea4ded3d5631ab885
Syed F Ahmad <furquan@protonmail.com>
2022-11-28 18:13:06 +0530

    Removed standard CSS className and added tailwind classes to the main container

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import TopBar from './components/TopBar';
 
 const App = () => {
   return (
-    <div className="App">
-      <TopBar />
-      <div className="text">Placeholder</div>
+    <div className="bg-mountains-blur bg-center bg-cover bg-no-repeat">
+      <div>
+        <TopBar />
+        <div className="font-sans font-bold text-white text-4xl hover:underline p-10 pb-96">Welcome!</div>
+      </div>
     </div>
   );
 };
