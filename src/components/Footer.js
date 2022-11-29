6993d48f39e0104bf7b6d9208ace355e7aa25ebc
Syed F Ahmad <furquan@protonmail.com>
2022-11-29 23:40:37 +0530

    Fixed responsive sizing for FiGithub icon by removing sm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => <div className="flex justify-between items-center bg-green-
   Copyright &#169; 2022 Syed Furquan Ahmad
   <Tooltip content="Check the source code">
     <a href="https://github.com/furquan-lp/simpledev.site">
-      <FiGithub className="mx-2 md:w-3/4 md:h-3/4 sm:w-fit sm:h-fit" />
+      <FiGithub className="mx-2 md:w-3/4 md:h-3/4 w-fit h-fit" />
     </a>
   </Tooltip>
 </div>;
