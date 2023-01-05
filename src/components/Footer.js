fc17618f67ce310773acbd94564d4fbb13b2d736
Syed F Ahmad <furquan@protonmail.com>
2023-01-05 11:16:42 +0530

    Make Footer text base only for md+ screens, smaller otherwise.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import { FiGithub } from 'react-icons/fi';
 import { Tooltip } from 'flowbite-react';
 
-const Footer = () => <div className="flex justify-between items-center bg-green-dark text-white p-1.5">
+const Footer = () => <div className="flex justify-between items-center bg-green-dark text-sm md:text-base
+text-white p-1.5">
   Powered by GitHub Pages. Copyright &#169; 2022 Syed Furquan Ahmad.
   <Tooltip content="Check the source code">
     <a href="https://github.com/furquan-lp/simpledev.site">
