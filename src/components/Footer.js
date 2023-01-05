1eede3f31af022a43315120f87563c000bf12307
Syed F Ahmad <furquan@protonmail.com>
2023-01-05 11:13:21 +0530

    Added GitHub Pages text on the Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { FiGithub } from 'react-icons/fi';
 import { Tooltip } from 'flowbite-react';
 
 const Footer = () => <div className="flex justify-between items-center bg-green-dark text-white p-1.5">
-  Copyright &#169; 2022 Syed Furquan Ahmad
+  Powered by GitHub Pages. Copyright &#169; 2022 Syed Furquan Ahmad.
   <Tooltip content="Check the source code">
     <a href="https://github.com/furquan-lp/simpledev.site">
       <FiGithub className="mx-2 md:w-3/4 md:h-3/4 w-fit h-fit" />
