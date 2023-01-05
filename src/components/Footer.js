93e9d9f64139941b9f5723f2b94c69576b0e9567
Syed F Ahmad <furquan@protonmail.com>
2023-01-05 14:42:53 +0530

    FiGithub icon size fixed to 2/3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ text-white p-1.5">
   Powered by GitHub Pages. Copyright &#169; 2022 Syed Furquan Ahmad.
   <Tooltip content="Check the source code">
     <a href="https://github.com/furquan-lp/simpledev.site">
-      <FiGithub className="mx-2 md:w-3/4 md:h-3/4 w-fit h-fit" />
+      <FiGithub className="mx-2 w-2/3 h-2/3" />
     </a>
   </Tooltip>
 </div>;
