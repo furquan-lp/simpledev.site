3be64b09ee12d32f5c801253a1b71b83879c10e5
Syed F Ahmad <furquan@protonmail.com>
2023-01-05 11:02:43 +0530

    Use minimal padding and full width for AboutText on screens smaller than md

--- src/components/Hi.js
+++ src/components/Hi.js
@@ -14,7 +14,7 @@ const AboutText = ({ about }) => {
     );
   } else {
     return (
-      <div className="fade-in-element transition-all delay-200 px-6 md:px py-2 md:py-8 w-1/2">
+      <div className="fade-in-element transition-all delay-200 px-px py-2 md:py-8 md:w-1/2 w-full">
         <div className="md:text-5xl text-2xl font-aboutfont italic">About Me</div>
         <div className="flex flex-col justify-between md:text-xl italic py-2">
           <div className="my-1">Hello! I'm Syed Ahmad and I like to build web applications. Ever since I remember coming
