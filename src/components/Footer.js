1ddc3fde91f314b9baa97e69e8bb1a67158ab859
Syed F Ahmad <furquan@protonmail.com>
2023-01-07 01:08:35 +0530

    Hide the 'Powered by GitHub Pages' text on screens smaller than md

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import { Tooltip } from 'flowbite-react';
 
 const Footer = () => <div className="flex justify-between items-center bg-green-dark text-sm md:text-base
 text-white p-1.5">
-  Powered by GitHub Pages. Copyright &#169; 2022 Syed Furquan Ahmad.
+  <div>
+    <span className="hidden md:inline">Powered by GitHub Pages.</span> Copyright &#169; 2022 Syed Furquan Ahmad.
+  </div>
   <Tooltip content="Check the source code">
     <a href="https://github.com/furquan-lp/simpledev.site">
       <FiGithub className="mx-2 w-2/3 h-2/3" />
