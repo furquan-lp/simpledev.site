2106990f43d81a3df896019cbfa6e0bd8ee42073
Syed F Ahmad <furquan@protonmail.com>
2023-01-13 00:20:48 +0530

    Added hover underline responsive styling to the LICENSE link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ import { Tooltip } from 'flowbite-react';
 const Footer = () => <div className="flex justify-between items-center bg-green-dark text-sm md:text-base
 text-white p-1.5">
   <div>
-    <span className="hidden md:inline">Powered by GitHub Pages.</span> Copyright &#169; 2022 Syed Furquan Ahmad.
+    <span className="hidden md:inline">Powered by GitHub Pages.</span> <a className="underline decoration-white
+    md:decoration-transparent md:hover:decoration-white transition-all duration-200"
+      href="https://github.com/furquan-lp/simpledev.site/blob/master/LICENSE">
+      Copyright &#169; 2022 Syed Furquan Ahmad.</a>
   </div>
   <Tooltip content="Check the source code">
     <a href="https://github.com/furquan-lp/simpledev.site">
