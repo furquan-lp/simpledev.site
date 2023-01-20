e1137ad96453354ead22cce3494bb05aa06517a2
Syed F Ahmad <furquan@protonmail.com>
2023-01-21 01:21:35 +0530

    Moved the entire App component to pages/Home and added routing using pages from pages/

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import preval from 'preval.macro';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes, Route, Link
+} from 'react-router-dom';
 
-import TopBar from './components/TopBar';
-import Welcome from './components/Welcome';
-import HiCards from './components/Hi';
-import Footer from './components/Footer';
+import { About } from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="bg-mountains-blur bg-center bg-cover bg-no-repeat">
-      <TopBar />
-      <div className="flex flex-col md:mx-12">
-        <Welcome />
-        <HiCards />
-        <div className="font-sans font-bold text-white text-center md:text-2xl text-lg lg:p-10 p-4 pt-20">This website
-          is built using ReactJS, Tailwind CSS and Flowbite. Check the source code <a
-            href="https://github.com/furquan-lp/simpledev.site/" className="underline hover:text-green-light">here</a>.
-          Report bugs <a href="https://github.com/furquan-lp/simpledev.site/issues"
-            className="underline hover:text-green-light">here</a>.
-          <p>Build {preval`module.exports = new Date().toISOString();`}</p>
-        </div>
-      </div>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
