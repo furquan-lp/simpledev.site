5388a410a504699007ef6d8620833ca7ef2ac805
Syed F Ahmad <furquan@protonmail.com>
2023-01-21 01:22:27 +0530

    Added a barebones About page component

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import TopBar from '../components/TopBar';
+import Footer from '../components/Footer';
+
+export const About = () => <div className="bg-mountains-blur bg-center bg-cover bg-no-repeat">
+  <TopBar />
+  <div className="flex flex-col md:mx-12">
+    <span className="font-bold text-4xl text-white">About:</span>
+  </div>
+  <Footer />
+</div>;
