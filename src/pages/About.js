a44abef33731e92f4296657d8fa785d17c5cf5dc
Syed F Ahmad <furquan@protonmail.com>
2023-01-22 23:32:15 +0530

    Pass the page prop for TopBar

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import TopBar from '../components/TopBar';
 import Footer from '../components/Footer';
 
 export const About = () => <div className="bg-mountains-blur bg-center bg-cover bg-no-repeat">
-  <TopBar />
+  <TopBar page="about" />
   <div className="flex flex-col md:mx-12">
     <span className="font-bold text-4xl text-white">About:</span>
   </div>
