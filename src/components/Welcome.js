4a3cef05e7f0ac3594581bc7befda0075b5ac633
Syed F Ahmad <furquan@protonmail.com>
2023-01-24 23:50:16 +0530

    Big Fix: Fixed WelcomeCarousel, CarouselCard and Carousel img styling, and added title, description, button links/text and images for the CarouselCards

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,53 +1,62 @@
 import { Carousel } from 'flowbite-react';
 
+import webenvImg from '../assets/webenv-screenshot.webp';
+import tctpImg from '../assets/tctp-screenshot-chrome.webp';
+import iotImg from '../assets/device.gif';
+import githubImg from '../assets/github-screenshot.webp';
+
 const CarouselCard = ({ title, text, buttonLink, buttonText, image }) =>
   <div className="flex mx-10 mb-5 justify-between p-5">
-    <div className="flex flex-col">
+    <div className="flex flex-col max-w-3xl mx-2">
       <div className="font-sans text-white font-bold text-4xl mb-10">{title}</div>
       <span className="font-sans text-white text-xl">{text}</span>
       <a
-        className="text-xl my-24 mr-auto p-2 pr-2 rounded-lg text-white bg-white/20 hover:bg-transparent hover:underline
-        transition-colors duration-200"
+        className="text-xl my-5 mr-auto p-2 pr-2 rounded-lg text-white bg-white/20 hover:bg-transparent
+         hover:underline transition-colors duration-200"
         href={buttonLink}
       >
         {buttonText}
       </a>
     </div>
-    <img className="w-7/12 max-w-full h-auto rounded-md shadow-lg" src={image} alt={title + " representative image"} />
+    <img className="md:w-7/12 w-1/2 h-auto rounded-md shadow-lg shadow-slate-600" src={image}
+      alt={`${title} screenshot"`} />
   </div>;
 
 const WelcomeCarousel = () =>
   <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
     <Carousel>
-      <CarouselCard title="Hello!"
-        text="lorem ipsum sit dolor amet"
+      <CarouselCard title="Check out my blog!"
+        text="TCTP.xyz (The Chip Tinker Project) is my blog written using Gatsby and Tailwind CSS, and hosted on a
+         Netlify backend. Thanks to Tailwind CSS it has a mobile first design and it uses Netlify CMS which helps me
+         push out blog posts without having to mess with the internal Markdown."
         buttonLink="https://tctp.xyz"
-        buttonText="Check it out"
-        image="https://raw.githubusercontent.com/furquan-lp/WebEnv/master/screenshot.png"
+        buttonText="Visit site"
+        image={tctpImg}
       />
-      <CarouselCard title="Hi!"
-        text="lorem ipsum sit dolor amet"
-        buttonLink="https://tctp.xyz"
-        buttonText="Check it out"
-        image="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+      <CarouselCard title="Check out my app!"
+        text="WebEnv is a React Native app which connects to an IoT backend to monitor and log environment data.
+         Available for Android, iOS and the web. The IoT backend is powered by an ESP32 microcontroller which exposes a
+         REST API over an HTTP server (no HTTPS support for now)."
+        buttonLink="http://webenv.simpledev.site"
+        buttonText="Check out the live demo"
+        image={webenvImg}
       />
-      <CarouselCard title="Hey!"
-        text="lorem ipsum sit dolor amet"
-        buttonLink="https://tctp.xyz"
-        buttonText="Check it out"
-        image="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+      <CarouselCard title="Check out my GitHub!"
+        text="If you want to check the source code for all my projects and want to see what I'm up to then head over
+         here. I commit frequently but no guarantees regarding code readability ;-)"
+        buttonLink="https://github.com/furquan-lp"
+        buttonText="Visit my GitHub"
+        image={githubImg}
       />
-      <CarouselCard title="Hoi!"
-        text="lorem ipsum sit dolor amet"
-        buttonLink="https://tctp.xyz"
-        buttonText="Check it out"
-        image="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-      />
-      <CarouselCard title="Ahoy!"
-        text="lorem ipsum sit dolor amet"
-        buttonLink="https://tctp.xyz"
-        buttonText="Check it out"
-        image="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+      <CarouselCard title="Arduino and IoT stuff"
+        text="Two years ago I was working on a project to log environmental data (air quality, temperature, humidity,
+           etc.) and display it to an LCD screen. Later on I ported the code over to C++, running it on an ESP32 board
+           which served the data over LAN using WiFi called 'envmon'. A minimal version which forwarded the data
+           over the Internet to a JSON database was used as the backend for WebEnv. This is its current (and most
+           likely final) iteration."
+        buttonLink="https://github.com/furquan-lp/environment-monitor"
+        buttonText="Check out the source code"
+        image={iotImg}
       />
     </Carousel>
   </div >;
