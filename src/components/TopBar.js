ef488b0b7c77c9416c539529ff66425ed7e15a5d
Syed F Ahmad <furquan@protonmail.com>
2023-01-25 16:43:13 +0530

    Moved the TopBar link buttons into a TopBarButton component with styling and external links support

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -11,40 +11,37 @@ const HomeLogo = ({ path }) =>
     </div>
   </div>;
 
+const TopBarButton = ({ link, linktext, extern, styles }) => {
+  const buttonStyle = {
+    link: styles ? `text-green-light ${styles}` : `text-green-light`,
+    li: `inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-blue-gray hover:rounded-md
+     transition-all duration-200`
+  };
+  if (extern) {
+    return <a href={link} className={buttonStyle.link}>
+      <li className={buttonStyle.li}>
+        {linktext}
+      </li>
+    </a>;
+  } else {
+    return <Link className={buttonStyle.link} to={link}>
+      <li className={buttonStyle.li}>
+        {linktext}
+      </li>
+    </Link>;
+  }
+};
+
 const TopBar = ({ page }) =>
-  <div className="flex flex-wrap md:flex-nowrap justify-between bg-green-dark text-white md:p-1 md:m-1.5 m-px rounded-md">
+  <div className="flex flex-wrap md:flex-nowrap justify-between bg-green-dark text-white md:p-1 md:m-1.5 m-px
+   rounded-md">
     <Link to="/"><HomeLogo path={page} /></Link>
     <ul className="flex items-center list-none text-sm md:text-base md:m-1 p-0">
-      <Link className="text-green-light hidden md:inline" to="/">
-        <li className="inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-blue-gray
-          hover:rounded-md transition-all duration-200">
-          Home
-        </li>
-      </Link>
-      <Link className="text-green-light" to="/about">
-        <li className="inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-blue-gray
-          hover:rounded-md transition-all duration-200">
-          About
-        </li>
-      </Link>
-      <a className="text-green-light" href="https://tctp.xyz/">
-        <li className="inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-blue-gray
-          hover:rounded-md transition-all duration-200">
-          Blog
-        </li>
-      </a>
-      <Link className="text-green-light" to="/portfolio">
-        <li className="inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-blue-gray
-          hover:rounded-md transition-all duration-200">
-          Portfolio
-        </li>
-      </Link>
-      <Link className="text-green-light" to="/contact">
-        <li className="inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-blue-gray
-          hover:rounded-md transition-all duration-200">
-          Contact
-        </li>
-      </Link>
+      <TopBarButton link="/" linktext="Home" styles="hidden md:inline" />
+      <TopBarButton link="/about" linktext="About" />
+      <TopBarButton link="https://tctp.xyz/" linktext="Blog" extern={true} />
+      <TopBarButton link="/portfolio" linktext="Portfolio" />
+      <TopBarButton link="/contact" linktext="Contact" />
     </ul>
   </div>;
 
