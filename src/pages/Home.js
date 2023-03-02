c717ed226b21b73aefff83c9018d1fb1b7b3ef24
Syed F Ahmad <furquan@protonmail.com>
2023-03-02 15:04:15 +0530

    Converted the single line about technologies used to a list of frameworks with their icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import preval from 'preval.macro';
+import { SiReact, SiTailwindcss } from 'react-icons/si';
+import { MdOutlineViewCarousel } from 'react-icons/md';
 
 import TopBar from '../components/TopBar';
 import Welcome from '../components/Welcome';
@@ -12,8 +14,15 @@ const Home = () => {
       <div className="flex flex-col md:mx-12">
         <Welcome />
         <HiCards />
-        <div className="font-sans font-bold text-white text-center md:text-2xl text-lg lg:p-10 p-4 pt-20">This website
-          is built using ReactJS, Tailwind CSS and Flowbite. Check the source code <a
+        <div className="font-sans font-bold text-white md:text-2xl text-lg mx-4 lg:p-10 p-4 ">This website
+          is built using:
+          <div className="flex flex-col mb-5 font-normal">
+            <span className="flex items-center px-2 py-1 md:px-4 md:py-2"><SiReact className="mr-1" />ReactJS</span>
+            <span className="flex items-center px-2 py-1 md:px-4 md:py-2"><SiTailwindcss className="mr-1" />Tailwind CSS</span>
+            <span className="flex items-center px-2 py-1 md:px-4 md:py-2"><MdOutlineViewCarousel className="mr-1" />Flowbite</span>
+          </div>
+
+          Check the source code <a
             href="https://github.com/furquan-lp/simpledev.site/" className="underline hover:text-green-light">here</a>.
           Report bugs <a href="https://github.com/furquan-lp/simpledev.site/issues"
             className="underline hover:text-green-light">here</a>.
